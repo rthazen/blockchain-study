ff78246e3ba2f0b3ff15c27cf386960ae3c5fc23
Reed Hazen <redhazen@gmail.cocm>
2024-08-05 13:31:31 -0700

    create new transactions

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,6 @@
 function Blockchain() {
-    this.chain = [];
-    this.newTransactions = [];
+  this.chain = [];
+  this.pendingTransactions = [];
 }
 
 // if wanted to build as a class that is fine, but it's pretty much just a constructor function with syntactic sugar on top
@@ -11,20 +11,44 @@ function Blockchain() {
 //     }
 // }
 
-Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
-    const newBlock = {
-        index: this.chain.length + 1,
-        timestamp: Date.now(),
-        transactions: this.newTransactions,
-        nonce: nonce, // proof of work, a number
-        hash: hash,
-        previousBlockHash: previousBlockHash
-    };
+Blockchain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockHash,
+  hash
+) {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.pendingTransactions,
+    nonce: nonce, // proof of work, a number
+    hash: hash,
+    previousBlockHash: previousBlockHash,
+  };
 
-    this.newTransactions = [];
-    this.chain.push(newBlock);
+  this.pendingTransactions = [];
+  this.chain.push(newBlock);
 
-    return newBlock;
-}
+  return newBlock;
+};
+
+Blockchain.prototype.getLastBlock = function () {
+  return this.chain[this.chain.length - 1];
+};
+
+Blockchain.prototype.createNewTransaction = function (
+  amount,
+  sender,
+  recipient
+) {
+  const newTransaction = {
+    amount: amount,
+    sender: sender,
+    recipient: recipient,
+  };
+
+  this.pendingTransactions.push(newTransaction);
+
+  return this.getLastBlock()["index"] + 1;
+};
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,9 +1,35 @@
-const Blockchain = require('./blockchain');
+const Blockchain = require("./blockchain");
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2389, 'IUQHIUERH887', 'HQEFIH82138374');
-bitcoin.createNewBlock(3123, 'HWEFJH8734982', 'JWHFI83981');
-bitcoin.createNewBlock(8786, 'DHJFJHFGW645625', 'ADHFGAJ364523764');
+bitcoin.createNewBlock(2389, "IUQHIUERH887", "HQEFIH82138374");
+// bitcoin.createNewBlock(3123, 'HWEFJH8734982', 'JWHFI83981');
+// bitcoin.createNewBlock(8786, 'DHJFJHFGW645625', 'ADHFGAJ364523764');
+
+bitcoin.createNewTransaction(
+  100,
+  "alexfw9eq87fw98fuhqhi",
+  "jennusvief9q74yr387r"
+);
+
+bitcoin.createNewBlock(3123, "HWEFJH8734982", "JWHFI83981");
+
+bitcoin.createNewTransaction(
+  50,
+  "alexfw9eq87fw98fuhqhi",
+  "jennusvief9q74yr387r"
+);
+bitcoin.createNewTransaction(
+  300,
+  "alexfw9eq87fw98fuhqhi",
+  "jennusvief9q74yr387r"
+);
+bitcoin.createNewTransaction(
+  2000,
+  "alexfw9eq87fw98fuhqhi",
+  "jennusvief9q74yr387r"
+);
+
+bitcoin.createNewBlock(8786, "DHJFJHFGW645625", "ADHFGAJ364523764");
 
 console.log(bitcoin);
